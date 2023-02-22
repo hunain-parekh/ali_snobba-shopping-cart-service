7aa4477f8bbbd33c2528d751730960169030ab2b
Hunain Parekh <hunain.parekh@xloopdigital.com>
2023-02-22 10:32:54 +0500

    initial commit

---
+++ src/test/java/com/ali_snobba/shoppingcartservice/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali_snobba.shoppingcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
